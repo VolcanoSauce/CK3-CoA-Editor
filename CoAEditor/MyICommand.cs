4603dba7bc02d439506285f90e4e100f4dc89b9d
VolcanoSauce <37091802+VolcanoSauce@users.noreply.github.com>
2021-04-07 16:54:28 -0700

    Initial Commit

---
+++ CoAEditor/MyICommand.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace CoAEditor {
+    public class MyICommand : ICommand {
+        Action _TargetExecuteMethod;
+        Func<bool> _TargetCanExecuteMethod;
+
+        public MyICommand(Action executeMethod) {
+            _TargetExecuteMethod = executeMethod;
+        }
+
+        public MyICommand(Action executeMethod, Func<bool> canExecuteMethod) {
+            _TargetExecuteMethod = executeMethod;
+            _TargetCanExecuteMethod = canExecuteMethod;
+        }
+
+        public void OnCanExecuteChanged() {
+            CanExecuteChanged(this, EventArgs.Empty);
+        }
+
+        bool ICommand.CanExecute(object parameter) {
+            if(_TargetCanExecuteMethod != null) {
+                return _TargetCanExecuteMethod();
+            }
+
+            if(_TargetExecuteMethod != null) {
+                return true;
+            }
+
+            return false;
+        }
+
+        // Should use weak refs if command instance lifetime is longer than lifetime of UI object that get hooked up to command
+        public event EventHandler CanExecuteChanged = delegate { };
+
+        void ICommand.Execute(object parameter) {
+            if (_TargetExecuteMethod != null)
+                _TargetExecuteMethod();
+        }
+    }
+}
---
+++ CoAEditor/VML/ViewModelLocator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.ComponentModel;
+
+namespace CoAEditor.VML {
+    public class ViewModelLocator {
+        public static bool GetAutoHookedUpViewModel(DependencyObject obj) {
+            return (bool)obj.GetValue(AutoHookedUpViewModelProperty);
+        }
+
+        public static void SetAutoHookedUpViewModel(DependencyObject obj, bool value) {
+            obj.SetValue(AutoHookedUpViewModelProperty, value);
+        }
+
+        public static readonly DependencyProperty AutoHookedUpViewModelProperty = DependencyProperty.RegisterAttached("AutoHookedUpViewModel", typeof(bool), typeof(ViewModelLocator), new PropertyMetadata(false, AutoHookedUpViewModelChanged));
+
+        private static void AutoHookedUpViewModelChanged(DependencyObject d, DependencyPropertyChangedEventArgs e) {
+            if (DesignerProperties.GetIsInDesignMode(d)) 
+                return;
+
+            var viewType = d.GetType();
+
+            string str = viewType.FullName;
+            str = str.Replace(".Views.", ".ViewModels.");
+
+            var viewTypeName = str;
+            var viewModelTypeName = viewTypeName + "Model";
+            var viewModelType = Type.GetType(viewModelTypeName);
+            var viewModel = Activator.CreateInstance(viewModelType);
+
+            ((FrameworkElement)d).DataContext = viewModel;
+        }
+    }
+}
